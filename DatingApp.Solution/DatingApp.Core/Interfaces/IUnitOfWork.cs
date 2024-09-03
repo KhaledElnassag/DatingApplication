f524da46ae8a227f3514d57c97142b10e15f8e2d
KhaledElnassag <khaledelnassag@gmail.com>
2024-09-03 20:08:16 +0300

    Add UNitOfWork

---
+++ DatingApp.Solution/DatingApp.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.Core.Models;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Core.Interfaces
+{
+	public interface IUnitOfWork:IAsyncDisposable
+	{
+		IGenericeRepository<T>? GetRepository<T>() where T:BaseEntity;
+		Task<int> CompleteAsync();
+	}
+}
---
+++ DatingApp.Solution/DatingApp.Repository/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using DatingApp.Core.Interfaces;
+using DatingApp.Core.Models;
+using DatingApp.Repository.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Repository.Repositories
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly ApplicationContext _Context;
+        private Dictionary<string,object> Repositories { get; set; }
+
+        public UnitOfWork(ApplicationContext context)
+        {
+			_Context = context;
+			Repositories = new Dictionary<string,object>();
+		}
+		public IGenericeRepository<T>? GetRepository<T>() where T : BaseEntity
+		{
+			string ClassName = typeof(T).Name;
+			if(!Repositories.ContainsKey(ClassName))
+				Repositories.Add(ClassName, new GenericRepository<T>(_Context));
+			return Repositories[ClassName] as IGenericeRepository<T>;
+		}
+		public async Task<int> CompleteAsync()
+		{
+			return await _Context.SaveChangesAsync();
+		}
+
+		public async ValueTask DisposeAsync()
+		{
+			 await _Context.DisposeAsync();
+		}
+
+		
+	}
+}
